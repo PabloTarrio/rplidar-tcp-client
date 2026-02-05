6563891d3815262d3c12e9f7c68f822462be3de5
Pablo Tarrio <ptarriog@gmail.com>
2026-02-05 11:35:36 +0100

    refactor: Reorganizar estructura a src-layout estándar
    - Mover setup.py y README a raíz del proyecto
    - Eliminar nivel redundante src/lidar_client/lidar_client/
    - Actualizar setup.py para usar find_packages(where='src')
    - Estructura final: src/lidar_client/{__init__.py,client.py}

--- setup.py
+++ setup.py
@@ -3,20 +3,19 @@ from setuptools import find_packages, setup
 setup(
     name="lidar_client",
     version="0.1.0",
-    author="Tu Nombre",
-    author_email="tu@email.com",
+    author="Pablo Tarrio",
+    author_email="pablo.tarrio@uie.edu",
     description="Cliente Python para recibir datos de RPLIDAR A1 vía TCP",
-    long_description=open("README.md").read() if open("README.md") else "",
+    long_description=open("README_lidar_client.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/tu-usuario/lidar_client",
-    packages=find_packages(),
+    url="https://github.com/PabloTarrio/create3-lidar-client",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=[
-        # No tiene dependencias externas, solo usa librerías estándar
-    ],
+    install_requires=[],
 )
